7d0e12faf1dc8b11ace080e7347769f08a78328e
gergelj <rudolf.gergelj@gmail.com>
2026-03-19 17:59:28 +0100

    pauz aleave building

--- src/pages/strojevi/StrojPregled.jsx
+++ src/pages/strojevi/StrojPregled.jsx
@@ -14,13 +14,50 @@ export default function StrojPregled() {
     }
 
     return (
-    <>
-    Ovdje će se vidjeti strojevi
-   <ul>
-    {strojevi && strojevi.map((stroj) =>
-         (<li> {stroj.naziv}</li>
-         ))}
-   </ul>    
+    <> <Table>
+            <thead>
+                <tr>
+                    <th>Naziv</th>
+                    <th>Trajanje</th>
+                    <th>Cijena</th>
+                    <th>Datum pokretanja</th>
+                    <th>Aktiv</th>
+                    <th>Akcija</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                {strojevi && strojevi.map((stroj)=>(
+                    <tr>
+                        <td>{stroj.naziv}</td>
+                        <td>{stroj.trajanje}</td>
+                        <td><NumericFormat
+                            value={stroj.cijena}
+                            displayType={'text'}
+                            thousandSeparator='.'
+                            decimalSeparator=','
+                            suffix={'€'}
+                            decimalScale={2}
+                            fixedDecimalScale
+                        />
+                        </td>
+                        <td>
+                            <FormatDatum datum={stroj.datumPokretanja}/>
+
+
+                        </td>
+                        <td>
+                            <GrValidate
+                                size={25}
+                                color={stroj.aktivan?'green':'red'}
+                                />
+                            
+                            </td>
+                        <td></td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
 
     </> 
     )
